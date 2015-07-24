63ad72d9caa70dcf3655ce2d86f47882dba6a26a
Haz111 <haz111pl@gmail.com>
2015-07-25 00:55:48 +0200

    deleted interfaces

--- core/src/com/metaminers/game/objects/IBuilding.java
+++
@@ -1,9 +0,0 @@
-package com.metaminers.game.objects;
-
-/**
- * Created by Tymoteusz on 2015-07-25.
- */
-public interface IBuilding {
-    int hp = 0;
-    void build();
-}
--- core/src/com/metaminers/game/objects/IHeroClass.java
+++
@@ -1,8 +0,0 @@
-package com.metaminers.game.objects;
-
-/**
- * Created by Tymoteusz on 2015-07-25.
- */
-public interface IHeroClass {
-    void attack(IEnemy enemy);
-}
