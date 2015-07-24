7a2fe139128a9413f41347887b63be3edac02dd4
Haz111 <haz111pl@gmail.com>
2015-07-24 23:39:19 +0200

    dodane przyciski

--- core/src/com/metaminers/game/screens/MenuScreen.java
+++ core/src/com/metaminers/game/screens/MenuScreen.java
@@ -1,19 +1,92 @@
 package com.metaminers.game.screens;
 
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+
+
+import java.awt.*;
 
 /**
  * Created by Hubert on 2015-07-24.
  */
 public class MenuScreen implements Screen {
+    TextButton level;
+    TextButton options;
+    TextButton aboutAuthors;
+    TextButton exit;
+    Stage stage;
+    Game game;
+
+    Texture buttonTexture;
+    TextureRegion buttonTextureRegion;
+    TextButton playButton;
+    TextButtonStyle style;
+    Skin skin;
+    BitmapFont font;
+
+
+    public MenuScreen(Game game) {
+        this.game = game;
+        font = new BitmapFont();
+        skin = new Skin();
+
+        buttonTexture = new Texture(Gdx.files.internal("buttons/exampleButton.png"));
+        buttonTexture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+
+        buttonTextureRegion = new TextureRegion(buttonTexture);
+
+        style = new TextButtonStyle();
+        style.up = new TextureRegionDrawable(buttonTextureRegion);
+        style.down = new TextureRegionDrawable(buttonTextureRegion);
+        style.font = font;
+        playButton = new TextButton("Play", style);
+        playButton.setBounds(Gdx.graphics.getWidth()/2 - 50, Gdx.graphics.getHeight()/2, 100, 40);
+
+//        TextButton level = new TextButton("Start game", style);
+        //TextButton options = new TextButton("Options", new Skin());
+        //TextButton aboutAuthors = new TextButton("About Authors", new Skin());
+        //TextButton exit = new TextButton("Exit", new Skin());\
+
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+
+        stage.addActor(playButton);
+
+
+    }
+
     @Override
     public void show() {
-        
+        playButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+//                super.clicked(event, x, y);
+                MenuScreen.this.game.setScreen(new BeforeGameScreen(game));
+            }
+        });
+
+        Gdx.input.setInputProcessor(stage);
     }
 
     @Override
     public void render(float delta) {
-
+        Gdx.gl.glClearColor(0,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act();
+        stage.draw();
     }
 
     @Override
@@ -38,6 +111,7 @@ public class MenuScreen implements Screen {
 
     @Override
     public void dispose() {
-
+        stage.dispose();
+        skin.dispose();
     }
 }
