4e65f6a7d6b7eab3e78f624fd4aca0de780e590a
Haz111 <haz111pl@gmail.com>
2015-07-24 22:13:57 +0200

    Dodane screens

---
+++ core/src/com/metaminers/game/screens/OptionsScreen.java
@@ -0,0 +1,43 @@
+package com.metaminers.game.screens;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by Hubert on 2015-07-24.
+ */
+public class OptionsScreen implements Screen {
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
