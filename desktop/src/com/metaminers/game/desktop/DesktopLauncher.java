2ae9d2c7084971cc653e833571fdf5a9af9ed5be
Haz111 <haz111pl@gmail.com>
2015-07-24 22:03:49 +0200

    initial commit #2

---
+++ desktop/src/com/metaminers/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.metaminers.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.metaminers.game.BraveMinerDefenderGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BraveMinerDefenderGame(), config);
+	}
+}
