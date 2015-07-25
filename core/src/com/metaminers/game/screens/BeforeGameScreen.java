723418310c7b00baac88c9f54b68c3ff5b65e15d
Haz111 <haz111pl@gmail.com>
2015-07-25 03:16:09 +0200

    added more gui

--- core/src/com/metaminers/game/screens/BeforeGameScreen.java
+++ core/src/com/metaminers/game/screens/BeforeGameScreen.java
@@ -3,14 +3,21 @@ package com.metaminers.game.screens;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Button.ButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField.TextFieldStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
@@ -18,48 +25,124 @@ import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
  * Created by Hubert on 2015-07-24.
  */
 public class BeforeGameScreen implements Screen {
-    TextButton level;
-    TextButton options;
-    TextButton aboutAuthors;
-    TextButton exit;
     Stage stage;
     Game game;
 
-    Texture buttonTexture;
-    TextureRegion buttonTextureRegion;
-    TextButton playButton;
-    TextButton.TextButtonStyle style;
-    Skin skin;
+    TextureRegion playButtonTextureRegion;
+    TextureRegion leftButtonTextureRegion;
+    TextureRegion rightButtonTextureRegion;
+
+    Texture playButtonTexture;
+    Texture leftButtonTexture;
+    Texture rightButtonTexture;
+
+    TextButtonStyle style;
+    ButtonStyle leftArrowStyle;
+    ButtonStyle rightArrowStyle;
+    LabelStyle labelStyle;
+    TextFieldStyle nameTextFieldStyle;
+
+    Label labelName;
+    Label labelCharacter;
+    Label labelVillage;
+    Label labelDifficulty;
+
+    TextField nameTextField;
+
     BitmapFont font;
+
+    Button[] arrowButtons;
+    TextButton playButton;
+
+    Texture cursorTexture;
+    TextureRegion cursorTextureRegion;
+    TextButtonStyle cursorStyle;
 
 
     public BeforeGameScreen(Game game) {
         this.game = game;
         font = new BitmapFont();
-        skin = new Skin();
+//        skin = new Skin();
+//        leftArrowSkin = new Skin();
+//        rightArrowSkin = new Skin();
 
-        buttonTexture = new Texture(Gdx.files.internal("buttons/exampleButton.png"));
-        buttonTexture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        playButtonTexture = new Texture(Gdx.files.internal("buttons/exampleButton.png"));
+        playButtonTexture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
 
-        buttonTextureRegion = new TextureRegion(buttonTexture);
+        leftButtonTexture = new Texture(Gdx.files.internal("buttons/leftButton.png"));
+        leftButtonTexture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+
+        rightButtonTexture = new Texture(Gdx.files.internal("buttons/rightButton.png"));
+        rightButtonTexture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+
+        playButtonTextureRegion = new TextureRegion(playButtonTexture);
+        leftButtonTextureRegion = new TextureRegion(leftButtonTexture);
+        rightButtonTextureRegion = new TextureRegion(rightButtonTexture);
 
         style = new TextButton.TextButtonStyle();
-        style.up = new TextureRegionDrawable(buttonTextureRegion);
-        style.down = new TextureRegionDrawable(buttonTextureRegion);
+        style.up = new TextureRegionDrawable(playButtonTextureRegion);
+        style.down = new TextureRegionDrawable(playButtonTextureRegion);
         style.font = font;
-        playButton = new TextButton("Playsfsdc", style);
+        playButton = new TextButton("Play", style);
+        playButton.setBounds(Gdx.graphics.getWidth() / 2 - 50, 80, 100, 40);
 
-//        TextButton level = new TextButton("Start game", style);
-        //TextButton options = new TextButton("Options", new Skin());
-        //TextButton aboutAuthors = new TextButton("About Authors", new Skin());
-        //TextButton exit = new TextButton("Exit", new Skin());\
+        leftArrowStyle = new ButtonStyle();
+        leftArrowStyle.up = new TextureRegionDrawable(leftButtonTextureRegion);
+        leftArrowStyle.down = new TextureRegionDrawable(leftButtonTextureRegion);
+
+        rightArrowStyle = new ButtonStyle();
+        rightArrowStyle.up = new TextureRegionDrawable(rightButtonTextureRegion);
+        rightArrowStyle.down = new TextureRegionDrawable(rightButtonTextureRegion);
 
         stage = new Stage();
         Gdx.input.setInputProcessor(stage);
 
+        arrowButtons = new Button[6];
+        int i = 0;
+        for (; i < 3; i++){
+            arrowButtons[i] = new Button(leftArrowStyle);
+            stage.addActor(arrowButtons[i]);
+        }
+        for (; i<6; i++) {
+            arrowButtons[i] = new Button(rightArrowStyle);
+            stage.addActor(arrowButtons[i]);
+        }
+        arrowButtons[0].setBounds(Gdx.graphics.getWidth() / 2 - 400, 550, 20, 20);
+        arrowButtons[1].setBounds(Gdx.graphics.getWidth() / 2 - 400, 350, 20, 20);
+        arrowButtons[2].setBounds(Gdx.graphics.getWidth() / 2 - 400, 150, 20, 20);
+        arrowButtons[3].setBounds(Gdx.graphics.getWidth() / 2 + 400, 550, 20, 20);
+        arrowButtons[4].setBounds(Gdx.graphics.getWidth() / 2 + 400, 350, 20, 20);
+        arrowButtons[5].setBounds(Gdx.graphics.getWidth() / 2 + 400, 150, 20, 20);
+
+        labelStyle = new LabelStyle(font, Color.WHITE);
+        labelName = new Label("Choose your name", labelStyle);
+        labelCharacter = new Label("Choose your character", labelStyle);
+        labelVillage = new Label("Choose your village", labelStyle);
+        labelDifficulty = new Label("Choose difficulty", labelStyle);
+
+        cursorTexture = new Texture(Gdx.files.internal("cursors/editCursor.png"));
+        cursorTexture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+        cursorTextureRegion = new TextureRegion(cursorTexture);
+
+        cursorStyle = new TextButtonStyle();
+        cursorStyle.up = new TextureRegionDrawable(cursorTextureRegion);
+
+//        TODO: zrobic ladny kursor
+        nameTextFieldStyle = new TextFieldStyle(font, Color.WHITE, cursorStyle.up, style.up, style.up);
+        nameTextField = new TextField("", nameTextFieldStyle);
+
+        labelName.setBounds(200, 700, 100, 20);
+        labelCharacter.setBounds(200, 600,100,20);
+        labelVillage.setBounds(200, 400, 100, 20);
+        labelDifficulty.setBounds(200, 200, 100, 20);
+        nameTextField.setBounds(400, 700, 300, 30);
+
         stage.addActor(playButton);
-
-
+        stage.addActor(labelName);
+        stage.addActor(labelCharacter);
+        stage.addActor(labelVillage);
+        stage.addActor(labelDifficulty);
+        stage.addActor(nameTextField);
     }
 
     @Override
