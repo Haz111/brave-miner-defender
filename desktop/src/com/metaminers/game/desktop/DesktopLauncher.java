a5f1b4d0024fdba140ca579a5356540047121043
Haz111 <haz111pl@gmail.com>
2015-07-26 15:00:04 +0200

    borderless full screen for desktop

--- desktop/src/com/metaminers/game/desktop/DesktopLauncher.java
+++ desktop/src/com/metaminers/game/desktop/DesktopLauncher.java
@@ -6,9 +6,11 @@ import com.metaminers.game.BraveMinerDefenderGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
+		System.setProperty("org.lwjgl.opengl.Window.undecorated", "true");
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 1024;
 		config.height = 768;
+		config.fullscreen = false;
 		new LwjglApplication(new BraveMinerDefenderGame(), config);
 	}
 }
